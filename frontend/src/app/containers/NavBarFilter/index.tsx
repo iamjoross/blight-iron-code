3b0588c7e8e9febda6be33501019a5498a07436c
Jose Ross Barredo <iamjoross@gmail.com>
2020-08-19 12:00:10 +0800

    Create NavbarFilter Component

---
+++ frontend/src/app/containers/NavBarFilter/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Logo } from './components/Logo';
+import { StyleConstants } from 'styles/StyleConstants';
+import { PageWrapper } from '../../components/PageWrapper';
+import { UserMenuPill } from './components/UserMenuPill';
+import { ShoppingCart } from './components/ShoppingCart';
+import { SearchBar } from './components/SearchBar';
+
+export function NavBarFilter() {
+  return (
+    <Wrapper>
+      <PageWrapper>
+        <Logo />
+        <SearchBar />
+        <RightWrapper>
+          <ShoppingCart style={{ marginLeft: 10 }} />
+          <UserMenuPill />
+        </RightWrapper>
+      </PageWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.header`
+  box-shadow: 0 1px 0 0 ${p => p.theme.borderLight};
+  height: ${StyleConstants.NAV_BAR_HEIGHT};
+  display: flex;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: ${p => p.theme.background};
+  z-index: 2;
+
+  @supports (backdrop-filter: blur(10px)) {
+    backdrop-filter: blur(10px);
+    background-color: ${p =>
+      p.theme.background.replace(
+        /rgba?(\(\s*\d+\s*,\s*\d+\s*,\s*\d+)(?:\s*,.+?)?\)/,
+        'rgba$1,0.75)',
+      )};
+  }
+
+  ${PageWrapper} {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+const RightWrapper = styled.div`
+  display: flex;
+`;
--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -14,6 +14,7 @@ import { GlobalStyle } from '../styles/global-styles';
 
 import { HomePage } from './containers/HomePage/Loadable';
 import { NotFoundPage } from './containers/NotFoundPage/Loadable';
+import { ListingsPage } from './containers/ListingsPage/Loadable';
 
 export function App() {
   return (
@@ -27,6 +28,11 @@ export function App() {
 
       <Switch>
         <Route exact path={process.env.PUBLIC_URL + '/'} component={HomePage} />
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + '/s'}
+          component={ListingsPage}
+        />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
