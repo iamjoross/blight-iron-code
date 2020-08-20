bb1621f371899b6ee97da411224288973d7f08b3
Jose Ross Barredo <iamjoross@gmail.com>
2020-08-20 13:57:02 +0800

    Add SideMenu

---
+++ frontend/src/app/containers/SideMenu/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { StyleConstants } from 'styles/StyleConstants';
+
+
+export function SideMenu() {
+  return (
+    <div>SideMenu here</div>
+  );
+}
+
