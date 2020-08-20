afcda13ddcd41e06f1a558def64718dacdd7c178
Jose Ross Barredo <iamjoross@gmail.com>
2020-08-20 13:58:55 +0800

    Add Header

---
+++ frontend/src/app/containers/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { StyleConstants } from 'styles/StyleConstants';
+
+
+export function Header() {
+  return (
+    <header>Header here</header>
+  );
+}
+
